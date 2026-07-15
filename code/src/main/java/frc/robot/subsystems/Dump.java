ef03a35e6a8ea54dd4380501ff62267713ae13be
Allen Cai <152452752+allencai11@users.noreply.github.com>
2026-07-14 20:16:43 -0500

    some basic setup

--- code/src/main/java/frc/robot/subsystems/Dump.java
+++ code/src/main/java/frc/robot/subsystems/Dump.java
@@ -4,6 +4,9 @@ import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.hardware.TalonFX;
 
+import static frc.robot.util.Constants.RioConstants.*;
+import static frc.robot.util.Constants.DumpConstants.*;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Dump extends SubsystemBase {
@@ -22,7 +25,21 @@ public class Dump extends SubsystemBase {
   private CurrentLimitsConfigs m_floorCurrentConfig;
 
   public Dump() {
+    m_leftRoller = new TalonFX(LEFT_ROLLER_ID, RIO_BUS);
+    m_leftRollerFollow = new TalonFX(LEFT_ROLLER_FOLLOWER_ID, RIO_BUS);
+    m_rightRoller = new TalonFX(RIGHT_ROLLER_ID, RIO_BUS);
+    m_rightRollerFollow = new TalonFX(RIGHT_ROLLER_FOLLOWER_ID, RIO_BUS);
+    m_floor = new TalonFX(FLOOR_ID, RIO_BUS);
+    m_floorFollow = new TalonFX(FLOOR_FOLLOWER_ID, RIO_BUS);
 
+    m_rollerCurrentConfig.withSupplyCurrentLimit(ROLLER_CURRENT_LIMIT).withSupplyCurrentLimitEnable(true)
+        .withStatorCurrentLimit(ROLLER_CURRENT_LIMIT).withStatorCurrentLimitEnable(true);
+
+    m_leftRollerConfig.Slot0.kP = ROLLER_KP;
+    m_leftRollerConfig.Slot0.kI = ROLLER_KI;
+    m_leftRollerConfig.Slot0.kD = ROLLER_KD;
+
+    m_rightRollerConfiguration = m_leftRollerConfig;
   }
 
   @Override
