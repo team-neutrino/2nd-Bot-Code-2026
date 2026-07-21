97563da5b41a2898c00818b2081545848bf437fb
neutrino <neutrino3928@outlook.com>
2026-07-21 18:31:02 -0500

    started

--- code/src/main/java/frc/robot/subsystems/Intake.java
+++ code/src/main/java/frc/robot/subsystems/Intake.java
@@ -1,8 +1,14 @@
 package frc.robot.subsystems;
 
+import com.ctre.phoenix6.hardware.TalonFX;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Intake extends SubsystemBase {
+  private TalonFX m_deployMotor;
+  private TalonFX m_rollerMotor;
+  private TalonFX m_rollerFollower;
+
   public Intake() {
 
   }
