1a79b130ecfe36e3cf7caa0c90004d8091591ae4
Alex M <889182mud@ames.k12.ia.us>
2026-08-25 18:38:57 -0500

    pr maybe

--- newCode/src/main/java/frc/robot/util/MatchState.java
+++ newCode/src/main/java/frc/robot/util/MatchState.java
@@ -1,5 +1,65 @@
 package frc.robot.util;
 
-public class MatchState {
+import edu.wpi.first.wpilibj.DriverStation;
 
+public class MatchState {
+    private boolean allianceWonAuto = false;
+
+    public void setAutoWinner(boolean weWon) {
+        allianceWonAuto = weWon;
+    }
+
+    public double getMatchTime() {
+        return DriverStation.getMatchTime();
+    }
+
+    public String getGameState() {
+        double time = DriverStation.getMatchTime();
+
+        if (DriverStation.isAutonomous())
+            return "AUTO";
+        if (time > 130)
+            return "TRANSITION";
+        if (time <= 30)
+            return "ENDGAME";
+        return "TELEOP";
+    }
+
+    public double getRemainingShiftTime() {
+        double time = DriverStation.getMatchTime();
+        String state = getGameState();
+
+        switch (state) {
+            case "AUTO":
+                return time;
+            case "TRANSITION":
+                return time - 130;
+            case "ENDGAME":
+                return time;
+            case "TELEOP":
+                double teleopElapsed = 130 - time;
+                return 25 - (teleopElapsed % 25);
+            default:
+                return 0;
+        }
+    }
+
+    public String getCurrentShiftName() {
+        if (!getGameState().equals("TELEOP"))
+            return "NONE";
+
+        double matchTime = DriverStation.getMatchTime();
+
+        if (matchTime > 130)
+            return "Transition";
+        if (matchTime > 105)
+            return "Shift 1";
+        if (matchTime > 80)
+            return "Shift 2";
+        if (matchTime > 55)
+            return "Shift 3";
+        if (matchTime > 30)
+            return "Shift 4";
+        return "End Game";
+    }
 }
