4e6284dac66b1e69d4b4b3adfd46dd4dc509d783
Hanna <hanna.k301@gmail.com>
2026-09-08 18:29:22 -0500

    rollers tested

--- newCode/src/main/java/frc/robot/util/Constants.java
+++ newCode/src/main/java/frc/robot/util/Constants.java
@@ -38,10 +38,10 @@ public class Constants {
         public static final int ROLLER_FOLLOWER_ID = 33;
 
         public static final double DEPLOY_CURRENT_LIMIT = 20.0; // Needs to be tested
-        public static final double ROLLER_CURRENT_LIMIT = 15.0; // Needs to be tested
+        public static final double ROLLER_CURRENT_LIMIT = 20.0; // Needs to be tested
 
-        public static final double INTAKE_VOLTAGE = -7.5; // Needs to be tested
-        public static final double OUTTAKE_VOLTAGE = 7.5; // Needs to be tested
+        public static final double INTAKE_VOLTAGE = -9; // Needs to be tested
+        public static final double OUTTAKE_VOLTAGE = 9; // Needs to be tested
 
         public static final double STARTING_POSITION = 0.0;
         public static final double DEPLOYED_POSITION = 14.0; // Needs to be tested
